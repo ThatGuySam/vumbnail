a6994373e922b1cc25238420db49e91284eaff78
Sam Carlton <ThatGuySam@users.noreply.github.com>
2020-06-04 14:21:49 -0500

    Create javascript.js

---
+++ api/javascript.js
@@ -0,0 +1,4 @@
+// Original: https://vercel.com/tootallnate/vercel-deno/d7owfvxvp/source?f=src%2Fapi%2Fjavascript.js
+export default (req) => {
+	req.respond({ body: 'Hello from JavaScript in Deno!' });
+}

fb5bc84c11ebc5c9b2404a9ebb7461a1a6b13e6b
Sam Carlton <sam@sam.lc>
2022-02-03 09:16:07 -0600

    Use api function to get url

--- helpers/get-ffmpeg-url.js
+++ helpers/get-ffmpeg-url.js
@@ -1,5 +1,6 @@
-import youtubedl from 'youtube-dl-exec'
+// import youtubedl from 'youtube-dl-exec'
 import urlParser from 'js-video-url-parser'
+import axios from 'axios'
 
 
 const providerDefaultOptions = {
@@ -12,10 +13,44 @@ const providerDefaultOptions = {
 }
 
 
+
+// https://github.com/saanuregh/youtube-dl-web/blob/master/components/entry.js#L25
+function reduceFormats ( youtubeDlInfo ) {
+    const targetFormat = youtubeDlInfo.format_id
+
+    return youtubeDlInfo.formats.reduce((a, c) => {
+        let x = {
+          id: c.format_id,
+          label: c.format,
+        };
+        if (youtubeDlInfo.extractor_key.toLowerCase() === "youtube") {
+          const j =
+            c.format.includes("audio") && c.format_id !== targetFormat.split("+")[1];
+          const k =
+            !c.format.includes("audio") &&
+            c.ext === "webm" &&
+            youtubeDlInfo.formats.filter((i) => i.format_note === c.format_note).length >= 2;
+          const l = c.vcodec !== "none" && c.acodec !== "none";
+          if (j || k || l) {
+            return a;
+          }
+          if (c.vcodec !== "none" && c.acodec === "none") {
+            x = {
+              id: `${c.format_id}+${targetFormat.split("+")[1]}`,
+              label: c.format,
+            };
+          }
+        }
+        return [...a, x];
+      }, [])
+}
+
+
 export async function getFfmpegUrl ( options = {} ) {
     // https://github.com/Zod-/jsVideoUrlParser#readme
     const { provider } = urlParser.parse(options.videoUrl)
 
+    // Get options for provider
     const defaultOptions = providerDefaultOptions[provider]
 
     const {
@@ -26,23 +61,16 @@ export async function getFfmpegUrl ( options = {} ) {
         ...options
     }
 
-    const output = await youtubedl( videoUrl , {
-        getUrl: true,
-        // https://askubuntu.com/questions/486297/how-to-select-video-quality-from-youtube-dl
-        format: `worstvideo[ext=${ extension }]`,
-        // dumpSingleJson: true,
-        // noWarnings: true,
-        // noCallHome: true,
-        // noCheckCertificate: true,
-        // preferFreeFormats: true,
-        youtubeSkipDashManifest: true,
-        // referer: 'https://example.com'
-    })
-    .catch(error => {
-        console.warn(`Error fetching video ${videoUrl}`, error)
-    })
+    const ytdlUrl = new URL(`${process.env.VERCEL_URL}/api/info?q=${ videoUrl }&f=worstvideo[ext=${ extension }]`)
 
-    // console.log( 'output', output )
+    // Get the video data
+    const { data: youtubeDlInfo } = await axios.get( ytdlUrl.href )
+        .catch(error => {
+            console.warn(`Error fetching video ${videoUrl}`, error)
+        })
 
-    return output
+    // console.log('youtubeDlInfo', youtubeDlInfo)
+    // console.log('url', youtubeDlInfo.url)
+
+    return youtubeDlInfo.url
 }

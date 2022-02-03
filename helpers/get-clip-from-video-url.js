f64abaea1e5c49f70eab2a5a72ad76a0d01291a7
Sam Carlton <sam@sam.lc>
2022-02-03 14:05:40 -0600

    Add experiemental MPD code and references

--- helpers/get-clip-from-video-url.js
+++ helpers/get-clip-from-video-url.js
@@ -18,6 +18,13 @@ export async function getClipFromVideoUrl ( videoUrl, options = {} ) {
         extension,
     })
 
+    // // Process MPD manifest (dash)
+    // // An FFmpeg Solution: https://video.stackexchange.com/a/25389
+    // // Youtube-dl MPD Handling: https://github.com/ytdl-org/youtube-dl/blob/1980ff4550a3f040fbc1e054d6b91013e9d8cb96/youtube_dl/extractor/common.py#L2074
+    // // Vimeo Handling: https://github.com/ytdl-org/youtube-dl/blob/1980ff4550a3f040fbc1e054d6b91013e9d8cb96/youtube_dl/extractor/vimeo.py
+    // if ( ffpemgUrl.includes('mpd') ) {
+    // }
+
     // Only download 5 seconds
     // https://unix.stackexchange.com/a/282413/255649
     // `ffmpeg -ss 00:00:15.00 -i "URL" -t 00:00:05.00 -c copy ./file.mp4`

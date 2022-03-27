71a02ecae60bd76293f9a9941bdfc30f0fe9bf01
Sam Carlton <sam@sam.lc>
2022-03-27 14:28:44 -0500

    Add next config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	eslint: {
+		dirs: ['.'],
+	},
+	poweredByHeader: false,
+	trailingSlash: true,
+	basePath: '',
+	// The starter code load resources from `public` folder with `router.basePath` in React components.
+	// So, the source code is "basePath-ready".
+	// You can remove `basePath` if you don't need it.
+	reactStrictMode: true,
+}

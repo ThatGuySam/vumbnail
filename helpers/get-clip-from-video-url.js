ea90e1fc91045787582676c5dd7c8d0b94f32fd3
Sam Carlton <sam@sam.lc>
2022-04-10 13:43:03 -0500

    Measure fetching and ffmpeg performance

--- helpers/get-clip-from-video-url.js
+++ helpers/get-clip-from-video-url.js
@@ -5,7 +5,7 @@ import { execa } from 'execa'
 
 import { getFfmpegUrl } from './get-ffmpeg-url.js'
 import { sendSuccessResponseMedia } from './send-response.js'
-
+import { performance } from './performance.js'
 
 
 
@@ -45,10 +45,16 @@ export async function getClipFromVideoUrl ( videoUrl, options = {} ) {
         extension = 'mp4',
     } = options
 
+    const urlFetchMarkerName = 'url-fetch'
+    performance.mark( urlFetchMarkerName )
+
     const ffpemgUrl = await getFfmpegUrl({
         videoUrl,
         extension,
     })
+
+    performance.measure( 'URL Fetch time', urlFetchMarkerName )
+    
 
     // // Process MPD manifest (dash)
     // // An FFmpeg Solution: https://video.stackexchange.com/a/25389
@@ -87,7 +93,7 @@ export async function getClipFromVideoUrl ( videoUrl, options = {} ) {
             // 'aac',
 
         '-movflags',
-        'frag_keyframe+empty_moov',
+            'frag_keyframe+empty_moov',
 
         // Set format
         '-f',
@@ -95,8 +101,20 @@ export async function getClipFromVideoUrl ( videoUrl, options = {} ) {
         '-'
     ]
 
+
+    const ffmpegMarkerName = 'ffmpeg-process'
+    performance.mark( ffmpegMarkerName )
+
+    performance.measure( 'FFmpeg time', ffmpegMarkerName )
+
     // Run command
     const ffmpegProcess = execa( pathToFfmpeg, ffmpegArgs )
+
+    // On pipe end 
+    ffmpegProcess.stdout.on( 'end', () => {
+        // console.log('ffmpegProcess.stdout.on( \'end\' )')
+        performance.measure( 'FFmpeg time', ffmpegMarkerName )
+    })
 
     return {
         fileStream: ffmpegProcess.stdout,

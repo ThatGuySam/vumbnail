b88803b100b1d2fa693d6da50d422f80bc3c73ba
Sam Carlton <sam@sam.lc>
2022-04-11 22:50:29 -0500

    Move examples mapping to helper

---
+++ helpers/map-examples-to-page.js
@@ -0,0 +1,40 @@
+export async function mapExamplesToPages ( allExamples ) {
+    const allExamplePages = await Promise.all( allExamples.map(async exampleEntry => {
+        // console.log( 'exampleEntry', exampleEntry )
+        // console.log( 'headers', await exampleEntry.getHeaders() )
+
+
+        return {
+            params: {
+                // Get slug from file path
+                slug: exampleEntry.file.split('/').at(-2),
+            },
+            props: {
+              // Use first header as title
+              title: (await exampleEntry.getHeaders())[0].text,
+
+              entry: exampleEntry,
+
+              nav: null
+            }
+        }
+    }) )
+
+    const nav = allExamplePages.map( examplePage => {
+
+        return {
+            label: examplePage.props.title,
+            href: `/examples/${examplePage.params.slug}`,
+            slug: examplePage.params.slug
+        }
+    })
+
+
+    return allExamplePages
+        //Add nav to each page
+        .map( page => {
+            page.props.nav = nav
+
+            return page
+        })
+}

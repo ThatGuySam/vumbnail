f1f83cfef0b0e2af0881986fb8a470d55aa1efef
Sam Carlton <sam@sam.lc>
2022-04-20 12:07:46 -0500

    Copy Shiki artifacts from node module

--- build.js
+++ build.js
@@ -3,8 +3,8 @@ import sharp from 'sharp'
 import pathToFfmpeg from 'ffmpeg-static'
 import { execa } from 'execa'
 
-import { 
-    svgTemplate, 
+import {
+    svgTemplate,
     errorMedia
 } from './helpers/send-response.js'
 
@@ -17,6 +17,28 @@ const {
 
 'Magic happens here';
 (async () => {
+
+    const shikiFolder = './public/shiki'
+
+    // https://github.com/shikijs/shiki-playground/blob/ddc3ce7577aca7696eee022da14f6004d24bf60c/package.json#L5
+    const foldersToCopy = [
+        'dist',
+        'languages',
+        'samples',
+        'themes'
+    ]
+
+    // Ensure that the shiki public folder exists
+    await fs.ensureDir( shikiFolder )
+
+    // Keep folders that we do want to copy
+    for ( const folder of foldersToCopy ) {
+        // Copy onig.wasm from shiki to the public folder
+        await fs.copy(
+            `./node_modules/shiki/${ folder }`,
+            `${ shikiFolder }/${ folder }`
+        )
+    }
 
 
     // Write SVG Error image
@@ -37,25 +59,25 @@ const {
     // Build error video from error svg
     // ffmpeg -r 1 -f image2 -s 1920x1080 -i error.svg -vcodec libx264 -crf 25  -pix_fmt yuv420p error.mp4
     // https://hamelot.io/visualization/using-ffmpeg-to-convert-a-set-of-images-into-a-video/
-    const ffmpegArgs = [ 
+    const ffmpegArgs = [
         '-r',
-        '1', 
-        '-f', 
+        '1',
+        '-f',
         'image2',
-        '-s', 
+        '-s',
         `${ mp4.width }x${ mp4.height }`,
         '-i',
-        png.path, 
-        '-vcodec', 
-        'libx264', 
-        '-crf', 
-        '25', 
-        '-pix_fmt', 
+        png.path,
+        '-vcodec',
+        'libx264',
+        '-crf',
+        '25',
+        '-pix_fmt',
         'yuv420p',
         mp4.path
     ]
 
-    
+
     console.log('Generating mp4 from png')
     // Delete mp4 file if it exists
     if ( fs.existsSync( mp4.path ) ) {

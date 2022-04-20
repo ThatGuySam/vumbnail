69ca2a055c8edf3959c1dd64242604a5384e71cf
Sam Carlton <sam@sam.lc>
2022-04-20 12:44:25 -0500

    Add highlighter helper

---
+++ helpers/highlighter.js
@@ -0,0 +1,28 @@
+
+// Adapted from https://github.com/shikijs/shiki-playground/blob/ddc3ce7577aca7696eee022da14f6004d24bf60c/src/highlighter.ts
+
+import { setWasm, setCDN, getHighlighter } from 'shiki'
+// https://github.com/shikijs/shiki-playground/blob/main/src/preload.ts
+// import { preloadedThemes, preloadedLangs } from './preload'
+
+setWasm('/shiki/dist/onigasm.wasm')
+setCDN('/shiki/')
+
+let highlighter
+
+export async function initHighlighter () {
+    if ( !!highlighter ) throw new Error('Highlighter already initialized')
+
+    highlighter = await getHighlighter({
+        themes: [ 'github-dark' ],
+        langs: [ 'html' ]
+    })
+
+    return highlighter
+}
+
+// export function getHighlighterInstance () {
+//     if ( !highlighter ) throw new Error('Highlighter not initialized')
+
+//     return highlighter
+// }

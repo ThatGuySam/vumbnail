22285e829dbc1412c25b2f4d75f152f0418c2b2b
Sam Carlton <sam@sam.lc>
2022-04-21 16:52:37 -0500

    Add Embed Builder link to nav

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -10,9 +10,12 @@
                     <a class="font-bold text-blue-500 hover:text-blue-800" href="/">Vumbnail</a>
                 </li>
                 <li class="mr-6">
-                    <a class="text-blue-500 hover:text-blue-800" href="#responsive-image">Examples</a>
+                    <a class="text-blue-500 hover:text-blue-800" href="/#responsive-image">Examples</a>
                 </li>
-                
+                <li class="mr-6">
+                    <a class="text-blue-500 hover:text-blue-800" href="/embed-builder">Embed Builder</a>
+                </li>
+
                 <!--
                 <li class="mr-6">
                     <a class="text-blue-500 hover:text-blue-800" href="#medium-image">Medium Example</a>

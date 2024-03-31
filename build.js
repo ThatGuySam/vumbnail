abbd14a8831919e1931ce7294a826106643c19bf
ThatGuySam <sam@sam.lc>
2024-03-31 16:29:01 -0500

    Move error media generation into scripts folder

--- build.js
+++ build.js
@@ -40,53 +40,6 @@ const {
         )
     }
 
-
-    // Write SVG Error image
-    console.log('Writing SVG image')
-    await fs.writeFile(
-        svg.path,
-        svgTemplate()
-    )
-
-    // Generate png from svg
-    console.log('Generating png from svg')
-    await sharp( svg.path )
-        .resize( png.width, png.height )
-        // https://github.com/lovell/sharp/blob/83bb6a45542cbced5d68a258070f256f6ca8cee3/docs/api-channel.md#examples
-        .removeAlpha()
-        .toFile( png.path )
-
-    // Build error video from error svg
-    // ffmpeg -r 1 -f image2 -s 1920x1080 -i error.svg -vcodec libx264 -crf 25  -pix_fmt yuv420p error.mp4
-    // https://hamelot.io/visualization/using-ffmpeg-to-convert-a-set-of-images-into-a-video/
-    const ffmpegArgs = [
-        '-r',
-        '1',
-        '-f',
-        'image2',
-        '-s',
-        `${ mp4.width }x${ mp4.height }`,
-        '-i',
-        png.path,
-        '-vcodec',
-        'libx264',
-        '-crf',
-        '25',
-        '-pix_fmt',
-        'yuv420p',
-        mp4.path
-    ]
-
-
-    console.log('Generating mp4 from png')
-    // Delete mp4 file if it exists
-    if ( fs.existsSync( mp4.path ) ) {
-        fs.unlinkSync( mp4.path )
-    }
-
-    // Generate mp4 from png
-    await execa( pathToFfmpeg, ffmpegArgs )
-
     console.log('Files generated successfully!')
 
     process.exit()

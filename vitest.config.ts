5847cb051a5cc8e662f164d97aa4ecddff1b7cb3
ThatGuySam <sam@sam.lc>
2024-04-27 19:23:19 -0500

    Clean up uneeded expect error

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    // @ts-expect-error - Not sure what's going on here
     plugins: [tsconfigPaths()]
 })

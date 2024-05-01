b59e32265f82f2296341ff4d457057be540edb01
ThatGuySam <sam@sam.lc>
2024-04-30 21:32:46 -0500

    Add misc rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,29 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-    astro: false,
-    vue: false,
+export default antfu(
+    {
+        astro: false,
+        vue: false,
+        typescript: {
+            // tsconfigPath: 'tsconfig.json',
+        },
 
-    stylistic: {
-        indent: 4,
+        stylistic: {
+            indent: 4,
+        },
+
+        ignores: [
+            'pnpm-workspace.yaml',
+        ],
     },
-
-    ignores: [
-        'pnpm-workspace.yaml',
-    ],
-})
+    // Misc
+    // If this section gets too big, consider moving to a separate config group
+    {
+        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+        rules: {
+            'test/prefer-lowercase-title': 'off',
+            'curly': ['error', 'all'],
+            'quotes': ['error', 'single'],
+        },
+    },
+)

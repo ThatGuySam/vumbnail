b952447bd70dc9426b991bae19026d50a372f44a
ThatGuySam <sam@sam.lc>
2024-05-02 23:51:34 -0500

    Add trimNonAlpha helper

---
+++ helpers/utils.ts
@@ -0,0 +1,22 @@
+function isAlpha ( char: string ): boolean {
+    const code = char.charCodeAt( 0 )
+    return ( code >= 65 && code <= 90 ) || ( code >= 97 && code <= 122 )
+}
+
+export function trimNonAlpha ( str: string ): string {
+    let start = 0
+    let end = str.length - 1
+
+    // Find the first non-alpha character from the start
+    while ( start <= end && !isAlpha( str[ start ] ) ) {
+        start++
+    }
+
+    // Find the first non-alpha character from the end
+    while ( end >= start && !isAlpha( str[ end ] ) ) {
+        end--
+    }
+
+    // Return the substring that excludes alpha characters from both ends
+    return str.slice( start, end + 1 )
+}
